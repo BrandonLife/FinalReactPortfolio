887da55153d7bb61663c10cff8e8e4f758c72fcb
Brandon Life <blife1119@gmail.com>
2023-10-17 15:23:31 -0400

    added home styles

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/Home.css'
+function Home() {
+  return (
+    <div className='home-container'>
+        <div className="card-home-container">
+        <div className='card'>
+            <div className='card-title'>
+                <h1><a href='#'>title</a></h1>
+            </div>
+            <div className='quick-overview'>
+                <p>overview</p>
+            </div>
+        </div> 
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
