ecbdbc5042c45fc2f8849e259ff9a915f6beef79
Brandon Life <blife1119@gmail.com>
2023-10-17 21:58:21 -0400

    fixed new project form

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import '../styles/Projects.css'
-function Projects() {
+function Projects({ cards }) {
+//   const cardDisplay = cards.map(card => {
+//     return (
+//       <div className='card'>
+//       <div className='card-title'>
+//           <h1><a href='#'>title</a></h1>
+//           </div>
+//           <div className='summary'>
+//           <span>Summary</span> 
+//           </div>
+//       </div> 
+//   )
+// })
   return (
     
       <div className="card-home-container">
@@ -8,14 +20,7 @@ function Projects() {
           <h1 className='projects-header'>Projects</h1>   
           </div>
          
-        <div className='card'>
-        <div className='card-title'>
-            <h1><a href='#'>title</a></h1>
-            </div>
-            <div className='summary'>
-            <span>Summary</span> 
-            </div>
-        </div> {/*end of card  */}
+        
 
         <div className='card'>
             <div className='card-title'>
--- src/views/newCard.js
+++ src/views/newCard.js
@@ -1,47 +1,33 @@
-import React, {useState} from "react"
-function NewCard() {
+import React from "react"
+import { useState } from "react"
 
-    const [formValue, setFomrValue] = useState({ title: "", overview: "", url: "" })
-    const handleInput = (e) => {
-        const { name, value } = e.target
-        setFomrValue({ ...formValue, [name]: value })
-    }
+
+function NewCard() {
+    const [data, setData] = useState({
+        title: '',
+        overview: '',
+        url:''
+    })     
     
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const allInputValue = { title: formValue.title, overview: formValue.overview, url: formValue.url }
-        
-        fetch('http://localhost:8080/cards/', {
-            method: "POST",
-            mode: 'cors',
-            headers: {
-                'content-type': 'application/json',
-                "Access-Control-Allow-Origin": 'http://localhost:3000'
-            },
-            body: JSON.stringify(allInputValue)
-        })
-            .then(response => response.json())
-            .then(data => data)
-    }  
         return (
             <div>
                 <h1>Add a project card</h1>
-                <form onSubmit={handleSubmit}>
+                <form method="POST" action="http://localhost:8080/cards">
                     <div className="input-container">
                         <div className="input-element">
                             <label htmlFor="title">Title</label>
-                            <input onChange={handleInput} value={formValue.title} type="text" id="title" name="title" />
+                            <input onChange={(e)=> setData({...data, title: e.target.value}) } value={data.title} type="text" id="title" name="title" />
                         </div>
                         <div className="input-element">
                             <label htmlFor="overview">Overview</label>
-                            <input onChange={handleInput} value={formValue.overview} type="text" id="overview" name="overview" />
+                            <input onChange={(e)=> setData({...data, overview: e.target.value}) } value={data.overview} type="text" id="overview" name="overview" />
                         </div>
                         <div className="input-element">
                             <label htmlFor="url">URL</label>
-                            <input onChange={handleInput} value={formValue.url} type="text" id="url" name="url" />
+                            <input onChange={(e)=> setData({...data, url: e.target.value}) } value={data.url} type="text" id="url" name="url" />
                         </div>
                     </div>{/* End of input container */}
-                    <input type="submit" />
+                    <input type="submit" value='Add project' />
                 </form>
             </div>
         )
